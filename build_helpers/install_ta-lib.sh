f76ab4feca3829ed7d6a26b88c86c8c5f0bbeb57
OlorinSledge <me@bronsonblackmore.com>
2021-09-14 13:26:25 +1000

    Fix for untar ta-lib source file

--- build_helpers/install_ta-lib.sh
+++ build_helpers/install_ta-lib.sh
@@ -5,7 +5,7 @@ else
 fi
 echo "Installing to ${INSTALL_LOC}"
 if [ ! -f "${INSTALL_LOC}/lib/libta_lib.a" ]; then
-  tar zxvf ta-lib-0.4.0-src.tar.gz
+  tar zxvf build_helpers/ta-lib-0.4.0-src.tar.gz
   cd ta-lib \
   && sed -i.bak "s|0.00000001|0.000000000000000001 |g" src/ta_func/ta_utility.h \
   && curl 'http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD' -o config.guess \
